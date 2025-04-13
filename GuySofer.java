7db361ada980b56b9ad45653836ce2716ae49fb6
GuySofer1997 <guys347@gmail.com>
2025-04-13 21:49:54 +0300

    Updated GuySofer.java with diffrent line

--- GuySofer.java
+++ GuySofer.java
@@ -1,7 +1,7 @@
 public class GuySofer {
     public static void main(String[] args) {
         for (int i = 1; i <= 3; i++) {
-            System.out.println("Guy Sofer");
+            System.out.println("I changed GuySofer java file");
         }
     }
 }
